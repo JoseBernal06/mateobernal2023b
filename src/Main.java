970077c15816d72c3d9788bf4f07de727a22933b
Jose Mateo Bernal <jose.bernal@epn.edu.ec>
2023-11-22 19:09:06 -0500

    segunda version.

--- src/Main.java
+++ src/Main.java
@@ -6,48 +6,62 @@ public class Main {
         Scanner entrada=new Scanner(System.in);
         Random random=new Random();
 
-        int numeroAleatorio=random.nextInt(5);
-        String[] palabras={"loro", "pez", "oso", "gato", "lobo"};
+        int numeroAleatorio=random.nextInt(10);
+        String[] palabras={"loro", "flor", "bote", "gato", "ceso", "hola", "sapo", "iran", "ceno", "beta"};
 
         String palabraAleatoria=palabras[numeroAleatorio];
         char letra;
         int aciertos=1;
         int fallas=1;
 
-        for (int i=0; i<6; i++){
-            System.out.println("\n\tTIENES "+(5-i)+" INTENTOS ");
-            System.out.print("Ingrese la letra para descubrir el animal: ");letra=entrada.next().charAt(0);
+        for (int i=0; i<10; i++){
+            System.out.println("\n\tINTENTO "+(i+1));
+            System.out.print("Ingrese una letra: ");letra=entrada.next().charAt(0);
 
             if (palabraAleatoria.indexOf(letra)==-1){
+
                 if (fallas==1){
                     System.out.println("0");
                 }
                 else if (fallas==2) {
                     System.out.println(" 0 ");
-                    System.out.println("/| ");
+                    System.out.println("/ ");
                 }
                 else if (fallas==3) {
                     System.out.println(" 0 ");
-                    System.out.println("/|\\");
-
+                    System.out.println("/| ");
                 }
                 else if (fallas==4) {
                     System.out.println(" 0 ");
                     System.out.println("/|\\");
-                    System.out.println("/ ");
 
                 }
                 else if (fallas==5) {
                     System.out.println(" 0 ");
                     System.out.println("/|\\");
+                    System.out.println("/ ");
+
+                }
+                else if (fallas==6) {
+                    System.out.println(" 0 ");
+                    System.out.println("/|\\");
                     System.out.println("/ \\");
-                    System.out.println("\n\tHAS PERDIDO: (");
+                }
+                else if (fallas==7) {
+                    System.out.println(" | ");
+                    System.out.println(" | ");
+                    System.out.println(" 0 ");
+                    System.out.println("/|\\");
+                    System.out.println("/ \\");
+                    System.out.println("\n\tHAS PERDIDO :(");
                     System.out.println("\tLa palabra era "+palabraAleatoria);
                     break;
                 }
-                fallas++;
+
+                fallas+=1;
 
             }else{
+
                 aciertos++;
                 System.out.println("Muy bien has acertado la letra...");
 
@@ -56,7 +70,6 @@ public class Main {
                     break;
                 }
             }
-
 
         }
 
